55a7cc924f043de4bfdf5f071b59a2655c28ec5a
Sara Baqla <missatrox44@gmail.com>
2023-04-07 14:46:24 -0500

    route guards and redirects

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 // components
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 //root component set up with different routes
 //3 pages: home, signup, login
 function App() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              {/* if user logged in, render home */}
+              {user && <Home />}
+              {!user && <Redirect to="/login/" />}
+            </Route>
+            <Route path="/signup">
+              {/* hide if signed in */}
+              {!user && <Signup />}
+              {user && <Redirect to="/" />}
+            </Route>
+            <Route path="/login">
+              {/* hide if signed in */}
+              {!user && <Login />}
+              {user && <Redirect to="/" />}
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
 
-export default App
+export default App;
