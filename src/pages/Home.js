a2af650e13c7eef2a06397da8c757c8e7361ba36
Sara Baqla <missatrox44@gmail.com>
2023-04-07 12:18:10 -0500

    getting documents

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import BookList from '../components/BookList'
-import BookForm from '../components/BookForm'
+import { useEffect, useState } from "react";
+import BookList from "../components/BookList";
+import BookForm from "../components/BookForm";
+
+import { db } from "../firebase/config";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function Home() {
-  //state of books -> array with title and id
-  const [books, setBooks] = useState([
-    { title: 'the name of the wind', id: 1 },
-    { title: 'the dragon reborn', id: 2 },
-    { title: 'the final empire', id: 3 },
-    { title: 'the way of kings', id: 4 }
-  ])
+  const [books, setBooks] = useState(null);
 
+  //fetch data from firestore
+  useEffect(() => {
+    // create reference to collection
+    //1st arg: database we want to connect to 
+    //2nd arg: name of collection
+    const ref = collection(db, 'books')
+
+    //reach to books collections
+    getDocs(ref)
+    .then((snapshot) => {
+      let results = []
+      snapshot.docs.forEach(doc => {
+        results.push({id: doc.id, ...doc.data()})
+      })
+      setBooks(results)
+    }) 
+  }, []);
 
   return (
     <div>
